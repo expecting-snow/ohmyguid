3641794872c4e1efcc751f17075aa103b868a770
Florian Eckert <florian.eckert@gmail.com>
2025-07-30 20:37:19 +0200

    .

--- src/GuidLinkProvider.ts
+++ src/GuidLinkProvider.ts
@@ -1,5 +1,3 @@
-
-import { MicrosoftEntraIdTenantInformation } from "./Models/MicrosoftEntraIdTenantInformation";
 import { GuidResolverResponse } from "./Models/GuidResolverResponse";
 
 export class GuidLinkProvider {
@@ -16,12 +14,20 @@ export class GuidLinkProvider {
                 return 'https://portal.azure.com/#view/Microsoft_Azure_Resources/ManagementGroupBrowseBlade/~/MGBrowse_overview';
             case 'Microsoft Entra ID AppRegistration':
                 return `https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/${item.object.appId}/isMSAApp~/false`;
+            case 'Microsoft Entra ID AppRegistration Details':
+                return `https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/${item.object.appRegistration.appId}/isMSAApp~/false`;
             case 'Microsoft Entra ID ServicePrincipal':
                 return `https://portal.azure.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/Overview/objectId/${item.object.id}/appId/${item.object.appId}`;
+            case 'Microsoft Entra ID ServicePrincipal Details':
+                return `https://portal.azure.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/Overview/objectId/${item.object.servicePrincipal.id}/appId/${item.object.servicePrincipal.appId}`;
             case 'Microsoft Entra ID Group':
                 return `https://portal.azure.com/#view/Microsoft_AAD_IAM/GroupDetailsMenuBlade/~/Overview/groupId/${item.guid}/menuId/`;
+            case 'Microsoft Entra ID Group Details':
+                return `https://portal.azure.com/#view/Microsoft_AAD_IAM/GroupDetailsMenuBlade/~/Overview/groupId/${item.group.guid}/menuId/`;
             case 'Microsoft Entra ID User':
                 return `https://portal.azure.com/#view/Microsoft_AAD_UsersAndTenants/UserProfileMenuBlade/~/overview/userId/${item.guid}/hidePreviewBanner~/true`;
+            case 'Microsoft Entra ID User Details':
+                return `https://portal.azure.com/#view/Microsoft_AAD_UsersAndTenants/UserProfileMenuBlade/~/overview/userId/${item.user.guid}/hidePreviewBanner~/true`;
             default:
                 console.log(`No link available for type: ${item.type}`);
                 return undefined;
