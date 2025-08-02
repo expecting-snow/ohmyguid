f0b40e87e5af676df75ebd43785736bb6826aed6
Florian Eckert <florian.eckert@gmail.com>
2025-08-02 05:07:37 +0200

    .

--- src/GuidResolverMicrosoftEntraId/GuidResolverMicrosoftEntraIdDirectoryObject.ts
+++ src/GuidResolverMicrosoftEntraId/GuidResolverMicrosoftEntraIdDirectoryObject.ts
@@ -14,32 +14,15 @@ export class GuidResolverMicrosoftEntraIdDirectoryObject extends GuidResolverMic
 
             if (response && response.displayName && response['@odata.type']) {
 
-                if (response["@odata.type"] === '#microsoft.graph.group') {
-                    abortController.abort();
-                    return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID Group', response, new Date());
-                }
-                else if (response["@odata.type"] === '#microsoft.graph.user') {
-                    abortController.abort();
-                    return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID User', response, new Date());
-                }
-                else if (response["@odata.type"] === '#microsoft.graph.servicePrincipal') {
-                    abortController.abort();
-                    return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID ServicePrincipal', response, new Date());
-                }
-                else if (response["@odata.type"] === '#microsoft.graph.application') {
-                    abortController.abort();
-                    return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID AppRegistration', response, new Date());
-                }
-                else if (response["@odata.type"] === '#microsoft.graph.tokenLifetimePolicy') {
-                    abortController.abort();
-                    return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID TokenLifetimePolicy', response, new Date());
-                }
-                else if (response["@odata.type"] === '#microsoft.graph.directoryRole') {
-                    abortController.abort();
-                    return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID DirectoryRole', response, new Date());
-                }
+                     if (response["@odata.type"] === '#microsoft.graph.group'              ) { abortController.abort(); return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID Group'              , response, new Date()); }
+                else if (response["@odata.type"] === '#microsoft.graph.user'               ) { abortController.abort(); return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID User'               , response, new Date()); }
+                else if (response["@odata.type"] === '#microsoft.graph.servicePrincipal'   ) { abortController.abort(); return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID ServicePrincipal'   , response, new Date()); }
+                else if (response["@odata.type"] === '#microsoft.graph.application'        ) { abortController.abort(); return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID AppRegistration'    , response, new Date()); }
+                else if (response["@odata.type"] === '#microsoft.graph.tokenLifetimePolicy') { abortController.abort(); return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID TokenLifetimePolicy', response, new Date()); }
+                else if (response["@odata.type"] === '#microsoft.graph.tokenIssuancePolicy') { abortController.abort(); return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID TokenIssuancePolicy', response, new Date()); }
+                else if (response["@odata.type"] === '#microsoft.graph.directoryRole'      ) { abortController.abort(); return new GuidResolverResponse(response.id, response.displayName, 'Microsoft Entra ID DirectoryRole'      , response, new Date()); }
                 else {
-                    console.warn(`Unknown response type: ${response["@odata.type"]} for id: ${response.id}`);
+                    console.log(`Unknown response type: ${response["@odata.type"]} for id: ${response.id}`);
                     return undefined;
                 }
             }
