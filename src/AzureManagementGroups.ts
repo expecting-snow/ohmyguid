672f9fa22dfc19b5b75c92a4a7fd79576fc724e3
Florian Eckert <florian.eckert@gmail.com>
2025-08-11 08:37:11 +0200

    .

--- src/AzureManagementGroups.ts
+++ src/AzureManagementGroups.ts
@@ -21,17 +21,41 @@ export class EntityNode {
         return this.descendants;
     }
 
-    flatten(){
-         return this.flattenInternal(this);
+    flatten() {
+        const flat = this.flattenInternal(this);
+
+        return this.deepCloneExclude(flat, ['entity', 'descendants']);;
     }
 
-    flattenInternal(node: EntityNode):any {
-        return {
-            [`${node.entity.displayName}`]: node.entity.id,
-            descendants: node.getDescendants().length > 0 
-                       ? node.getDescendants().map(descendant => this.flattenInternal(descendant)) 
-                       : undefined
-        };
+    flattenInternal(node: EntityNode): any {
+        const descendants = node.getDescendants();
+
+        if (descendants.length > 0) {
+            for (const descendant of descendants) {
+                (node as any)[`${descendant.entity.displayName}`] = this.flattenInternal(descendant);
+            }
+
+            return node;
+        }
+        else {
+
+            return node.entity.id;
+        }
+    }
+
+    deepCloneExclude<T extends object>(obj: T, exclude: string[]): T {
+        if (Array.isArray(obj)) {
+            return obj.map(item => this.deepCloneExclude(item, exclude)) as any;
+        } else if (obj && typeof obj === 'object') {
+            const result: any = {};
+            for (const key of Object.keys(obj)) {
+                if (!exclude.includes(key)) {
+                    result[key] = this.deepCloneExclude((obj as any)[key], exclude);
+                }
+            }
+            return result;
+        }
+        return obj;
     }
 }
 
